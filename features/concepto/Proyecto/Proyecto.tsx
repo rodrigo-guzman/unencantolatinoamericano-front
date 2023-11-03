dde12148032fa2bf29d8fef75e22e02597220d38
fedeguzani <112135630+fedeguzani@users.noreply.github.com>
2023-11-03 20:31:18 -0300

    Concepto OK y tipografia de Comercial

--- features/concepto/Proyecto/Proyecto.tsx
+++ features/concepto/Proyecto/Proyecto.tsx
@@ -23,7 +23,7 @@ const Proyecto: React.FC = () => {
                     adecuado.
                 </p>
                 {/* <Image src={'https://firebasestorage.googleapis.com/v0/b/un-encanto-latinoamericano.appspot.com/o/Proyecto-un-encanto.jpeg?alt=media&token=e1c2b337-e934-438b-a48f-081cc31e10c7&_gl=1*go0i50*_ga*MTQ4ODY2MzM3NS4xNjk0Mjk3MzIx*_ga_CW55HF8NVT*MTY5ODg4NDI3NC4zLjEuMTY5ODg4NTU2Ny42MC4wLjA.'} alt="Imagen" width={475} height={475} className={styles.image} /> */}
-                <Image src={''} alt="Imagen" width={475} height={475} className={styles.image} />
+                <Image src={''} alt="Imagen" width={375} height={375} className={styles.image} />
 
             </div>
         </div>
