a2fd2ae5fb5b683847f8783ed7deffd147be2318
fedeguzani <112135630+fedeguzani@users.noreply.github.com>
2023-11-04 07:00:41 -0300

    ultimos retoques

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
-/** @type {import('next').NextConfig} */
+//** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['firebasestorage.googleapis.com'],
     },
-    
+    output: {
+        // Ruta relativa a la carpeta del proyecto donde se exportará la aplicación.
+        // En tu caso, deseas exportar a la carpeta 'out'.
+        outputPath: 'out',
+    },
 };
 
 module.exports = nextConfig;
