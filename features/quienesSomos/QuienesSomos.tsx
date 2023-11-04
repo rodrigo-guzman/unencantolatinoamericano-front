8a29f396bc17c069fced96c74116d07d2c4b327b
fedeguzani <112135630+fedeguzani@users.noreply.github.com>
2023-11-04 02:02:36 -0300

    QuienesSomos ok

--- features/quienesSomos/QuienesSomos.tsx
+++ features/quienesSomos/QuienesSomos.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './QuienesSomos.module.css';
 import Image from 'next/image';
-import imagenQuienesSomos from '../../assets/QuienesSomos.png';
+
 
 const QuienesSomos: React.FC = () => {
     return (
 
         <div className={styles.contenedor}>
-            <Image src={''} alt="Imagen" width={855} height={819} className={styles.image} />
+            <Image src={'https://firebasestorage.googleapis.com/v0/b/un-encanto-latinoamericano.appspot.com/o/QuienesSomos.jpeg?alt=media&token=21ecf32f-f4f4-4fd9-b3dd-cc30061d54c6&_gl=1*u3pchn*_ga*MTQ4ODY2MzM3NS4xNjk0Mjk3MzIx*_ga_CW55HF8NVT*MTY5ODg5MDM0NS40LjEuMTY5ODg5MDU0NS42MC4wLjA.'} alt="Imagen" width={855} height={819} className={styles.image} /> 
             <div className={styles.columnaTexto}>
                 <h1 className={styles.h1Titulo}>Quienes Somos</h1>
                 <p>
@@ -21,7 +21,7 @@ const QuienesSomos: React.FC = () => {
                     comerciales creamos puentes que unen comunidades, artistas, marcas y amantes de la cultura latina en
                     una danza armoniosa de intercambio.
                 </p>
-                {/* <Image src={'https://firebasestorage.googleapis.com/v0/b/un-encanto-latinoamericano.appspot.com/o/QuienesSomos.jpeg?alt=media&token=21ecf32f-f4f4-4fd9-b3dd-cc30061d54c6&_gl=1*u3pchn*_ga*MTQ4ODY2MzM3NS4xNjk0Mjk3MzIx*_ga_CW55HF8NVT*MTY5ODg5MDM0NS40LjEuMTY5ODg5MDU0NS42MC4wLjA.'} alt="Imagen" width={855} height={819} className={styles.image} /> */}
+                
 
             </div>
         </div>
