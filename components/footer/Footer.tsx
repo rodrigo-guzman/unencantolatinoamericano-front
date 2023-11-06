e850ec20ded57148b8573eb3d54c98861e4d07c8
fedeguzani <112135630+fedeguzani@users.noreply.github.com>
2023-11-06 15:33:51 -0300

    Cambio de tel y mail de footer

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -10,7 +10,7 @@ const Footer: React.FC = () => {
             </div>
             <p>Registro de la propiedad intelectual número 123456</p>
             <div>
-                <p>Argentina | Teléfono: (351) 157123456 / (351) 158654987 | E-mail: nombre@unencantolatinoamericano.com</p>
+                <p>Argentina | Teléfono: (351) 153109717 / (351) 156099656 | E-mail: contacto@unencantolatinoamericano.com</p>
             </div>
             <div className={styles.socialMediaLinks}>
                 <SocialMediaLinks />
