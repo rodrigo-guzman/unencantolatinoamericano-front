3073fb20606c71aaf54349b1e5727567a3614d0b
fedeguzani <112135630+fedeguzani@users.noreply.github.com>
2023-11-21 17:23:31 -0300

    Cambios del header y ReCaptcha

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -8,14 +8,16 @@ const Header: React.FC = () => {
     return (
         <>
             <div className={styles.header}>
-            <div className={styles.logo}></div>
+                <div className={styles.logo}></div>
                 <div className={styles.navbar}>
                     
                     <Navbar />
-                    </div>
+                </div>
                 <div className={styles.content}>
                     <h1 className={styles.h1}>Un Encanto Latinoamericano</h1>
-                    <p className={styles.p}>Sinfonia de placeres</p>
+                    <div className={styles.pSandrina}>
+                        <p className={styles.p}>Sinfonia de placeres</p>
+                    </div>
                     <SocialMediaLinks />
                 </div>
             </div>
